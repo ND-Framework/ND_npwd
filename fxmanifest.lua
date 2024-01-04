08f8d510b3f67f41e31ac39c63944a63a3e5af6f
Marshular's Development <78507312+marshular@users.noreply.github.com>
2024-01-04 14:09:41 -0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,11 +2,13 @@
 
 author "Andyyy#7666"
 description "npwd integration with ND Framework"
-version "1.0.0"
+version "2.0.0"
 
 fx_version "cerulean"
 game "gta5"
 lua54 "yes"
+
+shared_script "@ND_Core/init.lua"
 
 server_scripts {
     "@oxmysql/lib/MySQL.lua",
