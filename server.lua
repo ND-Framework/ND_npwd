0ab0a7665660a5b144d28dce8af456ec1acb1840
Andy <86536434+Andyyy7666@users.noreply.github.com>
2024-01-06 05:14:39 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,5 +1,10 @@
 local phone = exports.npwd
 
+exports("pay", function(source, response, amount)
+    local player = NDCore.getPlayer(source)
+    return player and player.deductMoney("bank", amount, "Phone pay")
+end)
+
 local function createPhoneNumber(player)
     local phoneNumber = phone:generatePhoneNumber()
     player.setMetadata("phonenumber", phoneNumber)
@@ -8,10 +13,11 @@ end
 
 AddEventHandler("ND:characterLoaded", function(character)
     local src = character.source
-    MySQL.query.await("UPDATE nd_characters SET phone_number = ? WHERE identifier = ?", {character.getMetadata("phonenumber") or createPhoneNumber(character), character.identifier})
+    phone:unloadPlayer(src)
+
     phone:newPlayer({
         source = src,
-        identifier = character.identifier,
+        identifier = character.id,
         firstname = character.firstname,
         lastname = character.lastname,
         phoneNumber = character.getMetadata("phonenumber") or createPhoneNumber(character)
